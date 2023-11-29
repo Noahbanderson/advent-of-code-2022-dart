f5e405f7442ca3478fcb8127d4e0cbfa8f3a6fa4
Noah Anderson <noahbanderson@gmail.com>
2023-11-29 16:17:11 -0600

    Day10Part1 complete

--- lib/day_10.dart
+++ lib/day_10.dart
@@ -7,10 +7,44 @@ final input = getDayInput(10).readAsLinesSync();
 
 final _logger = Logger('Day10');
 
-void day10Part1() {
-  // TODO: Implement
+class Cpu {
+  var cycleCounter = 0;
+  var x = 1;
 
-  _logger.info('');
+  final void Function(int cycle, int x) onEachCycle;
+
+  Cpu({required this.onEachCycle});
+
+  void cycle() {
+    onEachCycle(++cycleCounter, x);
+  }
+}
+
+void day10Part1() {
+  var signalStrengthSum = 0;
+  final cpu = Cpu(onEachCycle: (final cycle, final x) {
+    if ([20, 60, 100, 140, 180, 220].contains(cycle)) {
+      signalStrengthSum += cycle * x;
+      _logger.fine('Major Cycle Hit! cycle: $cycle, x: $x -- total: ${cycle * x}');
+    }
+  });
+
+  for (final instruction in input) {
+    if (instruction == 'noop') {
+      cpu.cycle();
+      continue;
+    }
+
+    final v = int.parse(instruction.split(' ').last);
+
+    cpu.cycle();
+
+    cpu.cycle();
+
+    cpu.x += v;
+  }
+
+  _logger.info('signalStrengthSum: $signalStrengthSum');
 }
 
 void day10Part2() {
